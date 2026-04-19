545f1cb25c9156a48cd2f64607eea1da39b8ced1
nokey_exmaple2026 <db3812@columbia.edu>
2026-04-18 22:07:29 -0400

    Update asset paths in service worker

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE = 'kisanmitra-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/app.css',
-  '/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
+  '/kisan-mitra_App/',
+  '/kisan-mitra_App/index.html',
+ '/kisan-mitra_App/app-sunlight.css',
+'/kisan-mitra_App/app.js',
+'/kisan-mitra_App/manifest.json',
+'/kisan-mitra_App/icon-192.png',
+'/kisan-mitra_App/icon-512.png',
   'https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700&family=Noto+Sans+Devanagari:wght@400;500;600&display=swap'
 ];
 
